ee56051d1dbb2adbef651929fbc32379bf922609
Vaibhav <vaibhavagr11@gmail.com>
2021-07-21 00:08:23 +0530

    1st commit

---
+++ src/main/java/com/jenkinsspring/jenkinsspring/JenkinsspringApplication.java
@@ -0,0 +1,13 @@
+package com.jenkinsspring.jenkinsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsspringApplication.class, args);
+	}
+
+}
