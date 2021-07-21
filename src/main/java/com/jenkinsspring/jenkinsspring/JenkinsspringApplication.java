e96c342e29e7a65194de0661982a5e95afb3b69e
vaibhavagr11 <34593150+vaibhavagr11@users.noreply.github.com>
2021-07-21 18:40:15 +0530

    Update JenkinsspringApplication.java

--- src/main/java/com/jenkinsspring/jenkinsspring/JenkinsspringApplication.java
+++ src/main/java/com/jenkinsspring/jenkinsspring/JenkinsspringApplication.java
@@ -3,6 +3,7 @@ package com.jenkinsspring.jenkinsspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is my application
 @SpringBootApplication
 public class JenkinsspringApplication {
 
